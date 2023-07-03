5e542b2162161d11311f77115fe33f11ed218696
Oskar <obiwejnis@airnauts.com>
2023-07-03 07:30:27 -0700

    Basic elements on storyboard

--- firstProject/firstProject/ViewController.swift
+++ firstProject/firstProject/ViewController.swift
@@ -14,6 +14,14 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var textField1: UITextField!
+    @IBOutlet weak var label1: UILabel!
+    
+    @IBAction func onButton(_ sender: UIButton) {
+        
+        
+        label1.text = textField1.text
+    }
 }
 
