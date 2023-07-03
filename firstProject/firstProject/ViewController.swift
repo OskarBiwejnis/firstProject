69c5526abecdb03c1fbda82ef630c2e9a18e983e
Oskar <obiwejnis@airnauts.com>
2023-07-03 05:51:52 -0700

    Initial commit

---
+++ firstProject/firstProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  firstProject
+//
+//  Created by Oskar Biwejnis on 03/07/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
