fa6c68349190bda4d16449337b744cb936e5028a
Oskar <obiwejnis@airnauts.com>
2023-07-04 16:46:48 +0200

    Creating second screen in programmatic UI

--- firstProject/firstProject/FirstScreen.swift
+++ firstProject/firstProject/FirstScreen.swift
@@ -7,22 +7,113 @@
 
 import UIKit
 
-class FirstScreen: UIViewController {
+protocol WorkerDelegate: AnyObject {
+    func didFinishWork()
+}
 
+class FirstScreen: UIViewController, WorkerDelegate {
+    func didFinishWork() {
+        <#code#>
+    }
+    
+    
+    var label2: UILabel!
+    var button2: UIButton!
+    var textField2: UITextField!
+    var buttonToSecondScreen: UIButton!
+    
+    class Worker {
+        weak var delegate: WorkerDelegate?
+        
+        func startWork() {
+            delegate?.didFinishWork()
+        }
+    }
+    
+    private let worker = Worker()
+    
+    
+    
+    
+    
+    override func loadView() {
+        view = UIView()
+        view.backgroundColor = .white
+        
+        label2 = UILabel()
+        label2.translatesAutoresizingMaskIntoConstraints = false
+        label2.text = "LABEL"
+        view.addSubview(label2)
+        
+        textField2 = UITextField()
+        textField2.translatesAutoresizingMaskIntoConstraints = false
+        textField2.textAlignment = .center
+        textField2.text = "TEXT FIELD"
+        textField2.isUserInteractionEnabled = true
+        view.addSubview(textField2)
+        
+        button2 = UIButton(type: .system)
+        button2.translatesAutoresizingMaskIntoConstraints = false
+        button2.setTitle("BUTTON", for: .normal)
+        button2.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
+        view.addSubview(button2)
+        
+        buttonToSecondScreen = UIButton(type: .system)
+        buttonToSecondScreen.translatesAutoresizingMaskIntoConstraints = false
+        buttonToSecondScreen.setTitle("NEXT SCREEN", for: .normal)
+        buttonToSecondScreen.addTarget(self, action: #selector(buttonToSecondScreenTapped), for: .touchUpInside)
+        view.addSubview(buttonToSecondScreen)
+
+        
+        
+        NSLayoutConstraint.activate([
+            label2.centerXAnchor.constraint(equalTo: view.layoutMarginsGuide.centerXAnchor, constant: 0),
+            label2.bottomAnchor.constraint(equalTo: textField2.topAnchor, constant: -100),
+            
+            textField2.centerXAnchor.constraint(equalTo: view.layoutMarginsGuide.centerXAnchor, constant: 0),
+            textField2.centerYAnchor.constraint(equalTo: view.layoutMarginsGuide.centerYAnchor, constant: 0),
+            
+            button2.centerXAnchor.constraint(equalTo: view.layoutMarginsGuide.centerXAnchor, constant: 0),
+            button2.topAnchor.constraint(equalTo: textField2.bottomAnchor, constant: 50),
+            
+            buttonToSecondScreen.centerXAnchor.constraint(equalTo: view.layoutMarginsGuide.centerXAnchor, constant: 0),
+            buttonToSecondScreen.topAnchor.constraint(equalTo: button2.bottomAnchor, constant: 50)
+
+        ])
+    }
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemGray4
-        // Do any additional setup after loading the view.
+        
+        worker.delegate = self
+        
+        worker.startWork()
+    }
+    
+    @objc func buttonTapped(_ sender: UIButton) {
+        label2.text = textField2.text
+    }
+    
+    @objc func buttonToSecondScreenTapped(_ sender: UIButton) {
+        let secondScreen = SecondScreen()
+        navigationController?.pushViewController(secondScreen, animated: false)
     }
 
-    @IBOutlet weak var button1: UIButton!
+    /*@IBOutlet weak var button1: UIButton!
     @IBOutlet weak var textField1: UITextField!
     @IBOutlet weak var label1: UILabel!
     
     @IBAction func onButton(_ sender: UIButton) {
-        
-        
         label1.text = textField1.text
     }
+    */
+    
+    
+    
+    
+    
+    
 }
 
---
+++ firstProject/firstProject/SecondScreen.swift
@@ -0,0 +1,33 @@
+//
+//  SecondScreen.swift
+//  firstProject
+//
+//  Created by Oskar Biwejnis on 04/07/2023.
+//
+
+import UIKit
+
+class SecondScreen: UIViewController {
+
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemGreen
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
